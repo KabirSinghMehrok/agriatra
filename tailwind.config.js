3513a6257aaf2196fc7006f3a4288b7184d1df20
KabirSinghMehrok <mehrok.kabir.singh@gmail.com>
2023-01-07 18:00:36 +0530

    Add navbar, content and assets to landing.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,18 @@
 module.exports = {
   content: [
     "./src/**/*.{html,js}",
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/typography')],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('flowbite/plugin')
+  ],
+  variants : {
+    extend: {
+      display: ['group-focus']
+    },
+  },
 }
